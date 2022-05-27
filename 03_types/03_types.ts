8da23e0dfa140ab681a2a6fee3298602230cae72
yinna <yn@bvrft.com>
2022-05-27 17:34:33 +0800

    feat: 复杂类型的学习

--- 03_types/03_types.ts
+++ 03_types/03_types.ts
@@ -30,6 +30,9 @@ g = "hello"
 let h = '10';
 // h = f;  // 此时h也会变为any类型
 // h = g; // 会报错
+if (typeof(g) === 'string') {
+  h = g;
+}
 /* 
   类型断言，可以用来告诉解析器变量的实际类型。
   语法：
@@ -56,4 +59,62 @@ function fn2():never {
 
 
 // 复杂类型
+let z : object;
+z = {name: '小红'};
+z = function() {};
+z = [];
+
+
+/* 
+  {}  用于指定对象中可以包含哪些属性
+  语法：{属性名: 属性类型,属性名: 属性类型...}
+
+  在属性名后面加一个 ? 表示可选属性
+*/
+let y : { name : string, age?: number };
+y = { name: '小绿', age: 18 }
+
+
+// 用方括号表示属性名是字符串类型的任意属性
+let  x : { name : string, [propName: string] : unknown};
+x = { name: '小黄', age: 18, gender: '男'}
+
+
+/* 
+  设置函数结构的类型声明：
+      语法：(形参: 类型, 形参: 类型...) => 返回值
+*/
+let r : (a : number, b: number) => number
+r = function (n1,n2) {
+  return n1+n2;
+}
+
+
+/* 
+  声明数组：
+    方法一：   数组类型[]
+    方法二：   Array<数组类型>
+*/
+// 字符串数组
+let s : string[];
+s = ['1','2']
+// 数值数组
+let t : number[];
+t = [1,2]
+// 数值数组
+let  u : Array<string>;
+u = ['3','4']
+
+
+/* 
+  元组（）：表示一个固定长度的数组
+  语法：[类型, 类型, 类型]
+*/
+let v : [string, number]
+v = ['1', 3]
+
+
+/* 
+  枚举类型（enum）：
+*/
 
