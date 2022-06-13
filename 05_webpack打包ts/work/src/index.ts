c063e73562c518722d742ef0eeddafa5e84a8608
yinna <yn@bvrft.com>
2022-06-13 16:47:48 +0800

    feat: webpack基本配置

---
+++ 05_webpack打包ts/work/src/index.ts
@@ -0,0 +1,7 @@
+import {hi} from './test'
+
+function sum(a: number, b:number) {
+  return a+ b
+}
+let c = sum(1,23)
+console.log('c-----',c,hi)
--- 05_webpack打包ts/work/webpack.config.js
+++ 05_webpack打包ts/work/webpack.config.js
@@ -1,7 +1,14 @@
 // 引入包
-const path = require('path')
+const path = require('path');
+// 引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+// webpack-dev-server插件可以自动打开浏览器，并且实时更新
+// clean-webpack-plugin插件可以清除dist中旧的文件，将新的文件放进去
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 // webpack的所有配置都写在这里
 module.exports = {
+  mode: 'development',
   // 指定入口文件
   entry: './src/index.ts',
   // 指定打包后的文件所在的目录
@@ -12,7 +19,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   // 指定webapck打包时用到的模块
-  moudle: {
+  module: {
     // 指定要加载的规则
     rules: [
       {
@@ -24,5 +31,20 @@ module.exports = {
         exclude: /node-modules/
       }
     ]
+  },
+  // 配置webpack的插件
+  plugins: [
+    new CleanWebpackPlugin(),
+    // 自动生成html文件，并且自动引入资源
+    new HTMLWebpackPlugin(
+      {
+        // title: '这是一个自定义的title'
+        template: "./index.html"
+      }
+    )
+  ],
+  // 用来设置引用模块，不设置的话，引用数据将会报错
+  resolve: {
+    extensions: ['.ts','.js']
   }
 }
