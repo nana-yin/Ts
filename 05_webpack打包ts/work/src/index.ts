6cac50887cbbccccf0f13048c9eb51dc76466fff
yinna <yn@bvrft.com>
2022-06-13 18:03:33 +0800

    feat: webpack完整配置

--- 05_webpack打包ts/work/src/index.ts
+++ 05_webpack打包ts/work/src/index.ts
@@ -5,3 +5,20 @@ function sum(a: number, b:number) {
 }
 let c = sum(1,23)
 console.log('c-----',c,hi)
+
+
+let obj = {
+  name: '小名',
+  age: 18
+}
+obj.age = 24
+console.log('obj-----',obj)
+
+
+console.log(Promise)
+
+
+const arrow = (a: number, b:number) => {
+  return a+b
+}
+console.log('arrow',arrow)
--- 05_webpack打包ts/work/webpack.config.js
+++ 05_webpack打包ts/work/webpack.config.js
@@ -5,6 +5,9 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 // webpack-dev-server插件可以自动打开浏览器，并且实时更新
 // clean-webpack-plugin插件可以清除dist中旧的文件，将新的文件放进去
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// @babel-core
+// @babel/preset-env  使得代码兼容不同的浏览器的环境预设
+// core-js  模拟js运行环境，可以让老版本的浏览器使用新标准的技术
 
 // webpack的所有配置都写在这里
 module.exports = {
@@ -16,7 +19,11 @@ module.exports = {
     // 指定打包后文件的目录
     path: path.resolve(__dirname,'dist'),
     // 打包后文件的文件
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    // 告诉webpack，最外层不要使用箭头函数
+    environment: {
+      arrowFunction: false
+    }
   },
   // 指定webapck打包时用到的模块
   module: {
@@ -26,7 +33,36 @@ module.exports = {
         // test指定规则生效的文件
         test: /\.ts$/,
         // 用使用的loader
-        use: 'ts-loader',
+        use: [
+          // 配置babel
+          {
+            // 指定加载器
+            loader: 'babel-loader',
+            // 设置babel
+            options: {
+              // 设置预定义的环境
+              presets:[
+                [
+                  // 指定环境的插件
+                  "@babel/preset-env",
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      "chrome": "58",
+                      "ie": "11"
+                    },
+                    // 指定的corejs的版本
+                    "corejs": "3",
+                    // 使用corejs的方式，"usage"表示按需加载
+                    "useBuiltIns": "usage"
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
         // 要排除的文件
         exclude: /node-modules/
       }
