0edadcd8383f829e6e2a04dc67f97b761131e7ec
yinna <yn@bvrft.com>
2022-06-27 15:58:59 +0800

    feat: 新增蛇的类

---
+++ 07_snake/src/modules/gameControl.ts
@@ -0,0 +1,8 @@
+// import Food from './food'
+// import ScorePanel from './scorePanel'
+// import Snake from './snake'
+
+// 游戏的控制器
+export default class GameControl {
+
+}
--- 07_snake/src/modules/snake.ts
+++ 07_snake/src/modules/snake.ts
@@ -1,3 +1,41 @@
 export default class Snack{
-  
+  // 蛇的盒子元素
+  snakeEl: HTMLElement;
+  // 蛇的头元素
+  snakeHeadEl: HTMLElement;
+  // 蛇的身体元素
+  snakeBodyEl: HTMLCollection;
+
+
+  constructor() {
+    this.snakeEl = document.getElementById('snake')!;
+    this.snakeHeadEl = this.snakeEl.querySelector('#snake > div')! as HTMLElement;
+    this.snakeBodyEl = this.snakeEl.getElementsByTagName('div');
+  }
+
+  // 获取蛇头的x的位置
+  getSnakeHeadX() {
+    return this.snakeHeadEl.offsetLeft
+  }
+
+  // 获取蛇头的y的位置
+  getSnakeHeadY() {
+    return this.snakeHeadEl.offsetTop
+  }
+
+  // 设置蛇头的x的位置
+  setSnakeHeadX() {
+    this.snakeHeadEl.style.left = this.snakeHeadEl.offsetLeft +'px'
+  }
+
+  // 设置蛇头的y的位置
+  setSnakeHeadY() {
+    this.snakeHeadEl.style.top = this.snakeHeadEl.offsetTop +'px'
+  }
+
+  // 增加蛇的长度
+  addSnakeLength() {
+    this.snakeEl.insertAdjacentHTML('beforeend',"<div></div>")
+  }
+
 }
